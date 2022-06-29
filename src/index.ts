bf91a38165e3409f25f7355d73b66cc734a2efbe
Rahmonjon22 <rahmonjon2@yahoo.com>
2022-06-29 16:14:17 +0200

    first step

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+// tsc index.ts compiles the tsc into js
+// console.log('Hello World');
+
+// it specifies if it is number or string
+// let age: number = 20;
+
+// configuring the typescript compiler
+// tsc --init
+// edit tsconfig.json and add src file and add index.ts and write tsc in terminal
+
+
+
+// Debugging the Typescript applications
+
+let age: number = 20;
+if (age < 50)
+age += 10;
+console.log(age);
